c8d4f9c7d348751a32f96e1ed8556f55134c612b
Kevin Sebastian <93582300+mkevinsebastian@users.noreply.github.com>
2026-09-22 12:20:04 +0700

    New footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="pt-16 text-sm text-gray-500">
-      Last updated: 2 March, 2026
+      Last updated: 22 September, 2026
     </footer>
   );
 }
